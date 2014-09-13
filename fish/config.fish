ff57ba0f6d4d6ce14b49b04576be5d8f475d8604
Richard Johnson <github@rjohnson.id.au>
2014-09-13 21:10:42 +0100

    fix fonts

--- fish/config.fish
+++ fish/config.fish
@@ -11,9 +11,15 @@ set fish_plugins vi-mode python autojump
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
+#
+alias leaddb="mysql -uleadgen -h54.195.186.183 leadgen"
+alias alfred="mysql -urichard -halfred.skimlinks.com -p mugic"
+alias batman="mysql -urichard -hbatman.skimlinks.com -p mugic"
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+set fish_greeting ""
 
 . ~/virtualfish/virtual.fish
 . ~/virtualfish/auto_activation.fish
