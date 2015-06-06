84594bb765e8dbfd7fc37ff0b31f8cb942f8212c
Richard Johnson <github@rjohnson.id.au>
2015-06-06 17:15:08 +0100

    more updated

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -9,6 +9,7 @@ import XMonad.Layout.TwoPane
 import XMonad.Layout.LayoutScreens
 import XMonad.Config.Desktop (desktopLayoutModifiers)
 import XMonad.Hooks.ManageHelpers (isFullscreen,doFullFloat)
+import Graphics.X11.ExtraTypes.XF86
 
 main = xmonad $ defaultConfig
         { modMask = mod4Mask
@@ -24,6 +25,11 @@ main = xmonad $ defaultConfig
     philKeys = M.fromList $
               [ ((mod4Mask , xK_g) , spawn "google-chrome") 
               , ((mod4Mask , xK_p) , spawn "gmrun")
+              , ((noModMask, xF86XK_MonBrightnessUp) , spawn "xbacklight -inc 10")
+              , ((noModMask, xF86XK_MonBrightnessDown) , spawn "xbacklight -dec 10")
+              , ((noModMask, xF86XK_AudioMute) , spawn "amixer -D pulse set Master 1+ toggle")
+              , ((noModMask, xF86XK_AudioRaiseVolume) , spawn "amixer -D pulse sset Master 5%+")
+              , ((noModMask, xF86XK_AudioLowerVolume) , spawn "amixer -D pulse sset Master 5%-")
               , ((mod4Mask .|. shiftMask, xK_l) , spawn "gnome-screensaver-command -l && sleep 1; xset dpms force off")
               , ((mod4Mask , xK_i) , spawn "google-chrome --app='https://skim.hipchat.com/chat'")
               , ((mod4Mask , xK_s) , spawn "google-chrome --app='https://skimlinks.slack.com'") ]
