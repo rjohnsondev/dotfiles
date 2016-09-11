39bdbf75db569bf2d17df56f48e52c60cba7dc73
Richard Johnson <github@rjohnson.id.au>
2016-09-11 22:15:48 +1000

    random updates

--- omf/init.fish
+++ omf/init.fish
@@ -1,6 +1,6 @@
 
 # update paths
-set -x PATH /usr/local/bin ~/.cabal/bin ~/bin ~/apache-maven-3.3.9/bin /home/richard/gcloud/google-cloud-sdk/bin $PATH
+set -x PATH /usr/local/bin ~/.cabal/bin ~/bin ~/apache-maven-3.3.9/bin /home/richard/gcloud/google-cloud-sdk/bin ~/.rbenv/bin /usr/local/go/bin $PATH
 
 # use vi mode
 function fish_user_key_bindings
@@ -15,6 +15,8 @@ eval (python -m virtualfish)
 alias vim "nvim"
 
 complete --command gcloud --arguments="(/home/richard/gcloud/google-cloud-sdk/autocomplete.py (commandline -cp))"
+
+status --is-interactive; and . (rbenv init -|psub)
 
 # bass source '/home/richard/gcloud/google-cloud-sdk/path.bash.inc'
 # bass source '/home/richard/gcloud/google-cloud-sdk/completion.bash.inc'
